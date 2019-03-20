8b462128b0ce8bf6213d4a2f369f97f868db7afe
hulang <hulang6666@qq.com>
2019-03-20 23:11:05 +0800

    1.新增spring-boot-04-jpa知识项目

---
+++ spring-boot-04-jpa/src/main/java/com/niaobulashi/JpaApplication.java
@@ -0,0 +1,13 @@
+package com.niaobulashi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApplication.class, args);
+    }
+
+}
