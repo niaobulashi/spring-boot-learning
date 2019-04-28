d33cfcc65e4e46cb7db87919827e751daed97287
hulang <hulang6666@qq.com>
2019-04-28 23:56:09 +0800

    1.完善spring-boot-06-thymeleaf项目

---
+++ spring-boot-06-thymeleaf/src/main/java/com/niaobulashi/SpringBoot06ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.niaobulashi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot06ThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot06ThymeleafApplication.class, args);
+	}
+
+}
