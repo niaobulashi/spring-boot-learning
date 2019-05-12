24d4adf2870078fefb2910dd56faf0fc992dfb00
hulang <hulang6666@qq.com>
2019-05-12 09:59:43 +0800

    新增spring-boot-08-mail项目

---
+++ spring-boot-08-mail/src/main/java/com/niaobulashi/SpringBoot08MailApplication.java
@@ -0,0 +1,13 @@
+package com.niaobulashi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot08MailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot08MailApplication.class, args);
+	}
+
+}
